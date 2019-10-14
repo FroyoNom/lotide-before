7c5d4a6bafbf5d0aa8c8d966334fab623cfb023c
Simon <simonolsen388@gmail.com>
2019-10-14 22:32:47 +0000

    first commit

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`'Assertion Passed: ${actual} === ${expected}'`);
+  } else {
+    console.log(`'Assertion Failed: ${actual} !== ${expected}'`);
+  }
+};
+
+//Test Code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("hi", "hi");
+assertEqual(1, 1);
+assertEqual(1, 3);
