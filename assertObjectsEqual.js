de589750ad81d378a021036d8c416b9a3e3ebe9c
Simon <simonolsen388@gmail.com>
2019-10-16 23:59:18 +0000

    Added the 'util' library

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -17,10 +17,15 @@ const eqObjects = function(object1, object2) {
 };
 
 const assertObjectsEqual = function(actual, expected) {
+  const inspect = require("util").inspect;
   if (eqObjects(actual, expected)) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(
+      `Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`
+    );
   } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(
+      `Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`
+    );
   }
 };
 
