d44d45be528bb3a9f15731768e1c80332db22aa9
Simon <simonolsen388@gmail.com>
2019-10-21 16:39:57 +0000

    refactored

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require("../assertEqual");
+
+//Test
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Hello", "Hello");
+assertEqual(5, 10);
+assertEqual(2, 2);
+
+module.exports = assertEqual;
